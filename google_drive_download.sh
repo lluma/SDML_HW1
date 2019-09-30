246d737de87beb5f3b0cd6cb84f6a6440c0ec687
kjwang <rr123789456tw@gmail.com>
2019-09-30 20:19:25 +0800

    change glove zip file to extracted txt file with 300B word embedding and create data folder to catch all of them

--- google_drive_download.sh
+++ google_drive_download.sh
@@ -4,13 +4,16 @@ gdrive_download () {
   wget --no-check-certificate -r "https://docs.google.com/uc?export=download&id=$file_id" -O $output_filename
 }
 
+echo 'create data folder'
+mkdir data
+
 echo 'Downloading some zipped files......'
 
 echo 'Downloading pretrained embedding words from GloVe...'
-gdrive_download 1EQ3Z5lRI4TmiTJMJyovT_AcKXZ5xvIgm glove.6B.zip
+gdrive_download 1quJXyFoy3jMuO9mHr1ze8xuMF2T_VXcw data/glove.6B.zip
 
 echo 'Downloading training set...'
-gdrive_download 1U0Ay9jzr4rQJGOjhvH7918NFAv3HyVev task2_trainset.zip
+gdrive_download 1U0Ay9jzr4rQJGOjhvH7918NFAv3HyVev data/task2_trainset.zip
 
 echo 'Downloading public testing set'
-gdrive_downloading 1tYLP3j1RrbcfOvL8QnMpz1blbq5EqqSY task2_public_testset.zip
+gdrive_downloading 1tYLP3j1RrbcfOvL8QnMpz1blbq5EqqSY data/task2_public_testset.zip
